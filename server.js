167b65641c255034cf9fc01ca994923a6f051f04
Abdul Rahman M <145901885+Ar141204@users.noreply.github.com>
2025-08-16 22:29:45 +0530

    Update server.js

--- server.js
+++ server.js
@@ -253,6 +253,6 @@ app.get('/api/health', (req, res) => {
   res.json({ status: 'OK', message: 'Server is running' });
 });
 
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
